913dd67c023ca381d2a95901437d7d3331917e78
Your Name <you@example.com>
2021-10-13 13:45:14 +0900

    Upload all

---
+++ Basic_100/scan03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float f;
+
+	scanf("%f", &f);
+	printf("%f", f);
+	return (0);
+}
---
+++ Basic_100/scan05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char x;
+	char y;
+
+	scanf("%c %c", &x, &y);
+	printf("%c %c", y, x);
+	return (0);
+}
